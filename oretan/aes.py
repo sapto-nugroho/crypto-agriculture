b3a8989f19b88c32cd1f52e0badd0dcccc60410e
Nyayu Chika Marselina <nyayuchikamsln@gmail.com>
2026-05-13 18:55:27 +0700

    update aes.py

--- oretan/aes.py
+++ oretan/aes.py
@@ -1,7 +1,6 @@
 #ya Allah mau seblak
 
 import os
-import base64
 from cryptography.hazmat.primitives.ciphers.aead import AESGCM
 from cryptography.exceptions import InvalidTag
 
@@ -14,7 +13,7 @@ print(key.hex())
 
 #2 task AES-GCM:
 #Enkripsi data - supaya plaintext ga bisa dibaca
-#Buat tag - adversary ada ngubah ciphertext di tengah jalan atau engga (Man in The Middle)
+#Buat tag - u/ cek apakah adversary ada ngubah ciphertext di tengah jalan atau engga (Man in The Middle)
 #Karena ada tag, alhasil jadi IND-CCA 
 # IND-CCA: walaupun adversary dikasih fungsi u/ dekrip, tetap ga bisa bedain mana plaintext(?)
 
@@ -26,4 +25,27 @@ def encrypt(key, plaintext):
     #IGO MWOYAAAAA AAAAA
     nonce = os.urandom(12)
     aesgcm = AESGCM(key)
-    #Tag: untuk cek apakah data diutak-atik
+    #Tag: untuk cek apakah data diutak-atik oleh adversary
+    #Hasil ciphertext_tag = ct + tag, sheesh idk, lemme check later
+    #but claude said that the result of aesgcm will be the combination of ciphertext + tag
+    ciphertext_tag = aesgcm.encrypt(nonce, plaintext, None)
+    #Pisahkan ciphertext dan tag
+    ciphertext = ciphertext_tag[:-16] #buang yg 16 terakhir karna itu tag
+    tag = ciphertext_tag[-16:]
+    #Return dalam bentuk heksa
+    return{
+        "nonce": nonce.hex(),
+        "ciphertext": ciphertext.hex(),
+        "tag": tag.hex()
+    }
+
+#Dekripsi
+def decrypt(key, nonce_decrypt, ciphertext_decrypt, tag_decrypt):
+    nonce = bytes.fromhex(nonce_decrypt)
+    ciphertext = bytes.fromhex(ciphertext_decrypt)
+    tag = bytes.fromhex(tag_decrypt)
+    aesgcm = AESGCM(key)
+    #Di dalam dekripsi ada verifikasi tag
+    #Kalau tag tidak valid, nanti InvalidTag (oh god, wait a minute...)
+    plaintext = aesgcm.decrypt(nonce, ciphertext + tag, None)
+    return plaintext
